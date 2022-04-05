24663f7d434ef97082ad69ac8829dd99da863eb4
lnuvy <hangeulgod@naver.com>
2022-04-06 01:51:34 +0900

    미디어쿼리 어떻게 적용해야되지..?

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Grid, Image, Text } from "../elements";
 import { history } from "../redux/configureStore";
+import { changeTime } from "../shared/ChangeTime";
 
 const Card = (props) => {
-  const { image_url = "", user_name = "", post_id = null } = props;
+  const {
+    image_url = "",
+    user_name = "",
+    post_id = null,
+    insert_dt = "2020-01-01 13:00:00",
+  } = props;
+
   return (
     <Grid
       _onClick={() => {
@@ -13,14 +20,13 @@ const Card = (props) => {
       is_flex
       bg="#fff"
     >
-      <Grid width="auto" margin="0 8px 0 0">
+      <Grid is_flex width="auto" margin="0 16px 0 0">
         <Image size={80} shape="asdf" src={image_url} />
-      </Grid>
-      <Grid>
-        <Text>
+        <Text margin="0 0 0 16px">
           <b>{user_name}</b>님이 게시글에 댓글을 남겼스빈다.
         </Text>
       </Grid>
+      <Text margin="0 20px">{changeTime(insert_dt)}</Text>
     </Grid>
   );
 };
